412900fb4182e150af34b4839d89f55a3b858dc2
cruelsmith <92088441+cruelsmith@users.noreply.github.com>
2022-09-26 09:32:13 +0200

    check_puppetdb_node.rb fixes for robocop

--- check_puppetdb_node.rb
+++ check_puppetdb_node.rb
@@ -33,12 +33,12 @@ def api_call(query, opt = {})
     end
   end
 
-  request  = Net::HTTP::Get.new(uri.request_uri, {'Accept' => 'application/json'})
+  request  = Net::HTTP::Get.new(uri.request_uri, { 'Accept' => 'application/json' })
   response = http.request(request)
-  JSON.load(response.body)
+  JSON.parse(response.body)
 end
 
-@options  = Hash.new
+@options = {}
 OptionParser.new do |opts|
   opts.on('-n NODE', String, 'Node that will be searched for') { |node| @options[:node] = node }
   opts.on('-p PUPPETBOARD', String, 'FQDN of the puppetboard used for the link in the return message.') { |puppetboard| @options[:puppetboard] = puppetboard }
@@ -56,7 +56,7 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-puppet_node        = api_call(query="pdb/query/v4/nodes/#{@options[:node]}", opt=@options)
+puppet_node        = api_call(query = "pdb/query/v4/nodes/#{@options[:node]}", opt = @options)
 latest_report_hash = puppet_node['latest_report_hash']   || nil # do we have a report for this node?
 last_catalog       = puppet_node['catalog_timestamp']    || nil # when was the last catalog for this node compiled?
 last_status        = puppet_node['latest_report_status'] || nil # what was the last status from this node?
@@ -66,33 +66,31 @@ if error.nil?
   time_now = Time.now.utc.to_i
   time_cat = Time.parse(last_catalog).to_i
   time_dif = time_now - time_cat
-  time_24h = 86400
+  time_24h = 86_400
 end
 
 if last_catalog.nil? || latest_report_hash.nil?
   message = 'No catalog found' if error.nil?
   message = error unless error.nil?
   status  = [3, 'UNKNOWN']
+elsif @options.key?(:ttl) && time_dif > (@options[:ttl] * 0.9)
+  mm, ss = (@options[:ttl] - time_dif).divmod(60)
+  hh, mm = mm.divmod(60)
+  dd, hh = hh.divmod(24)
+  message = "Node will be expire in #{dd} days, #{hh} hours, #{mm} minutes, #{ss} seconds because Puppet hadn't run and a node-ttl of #{@options[:ttl]}s"
+  status  = [2, 'CRITICAL']
+elsif time_dif > time_24h
+  message = "Puppet hasn't run in 24h! - Last status: #{last_status}"
+  message = "Puppet hasn't run in 24h! - Last status: #{last_status}\nPlease check https://#{@options[:puppetboard]}/node/#{@options[:node]}" if last_status == 'failed'
+  status  = [1, 'WARNING'] if last_status == 'unchanged'
+  status  = [1, 'WARNING'] if last_status == 'changed'
+  status  = [2, 'CRITICAL'] if last_status == 'failed'
 else
-  if @options.key?(:ttl) && time_dif > ( @options[:ttl] * 0.9 )
-    mm, ss = (@options[:ttl] - time_dif).divmod(60)
-    hh, mm = mm.divmod(60)
-    dd, hh = hh.divmod(24)
-    message = "Node will be expire in #{dd} days, #{hh} hours, #{ss} seconds because Puppet hadn't run and a node-ttl of #{@options[:ttl]}s"
-    status  = [2, 'CRITICAL']
-  elsif time_dif > time_24h
-    message = "Puppet hasn't run in 24h! - Last status: #{last_status}"
-    message = "Puppet hasn't run in 24h! - Last status: #{last_status}\nPlease check https://#{@options[:puppetboard]}/node/#{@options[:node]}" if last_status == 'failed'
-    status  = [1, 'WARNING'] if last_status == 'unchanged'
-    status  = [1, 'WARNING'] if last_status == 'changed'
-    status  = [2, 'CRITICAL'] if last_status == 'failed'
-  else
-    message = "Last run at #{Time.parse(last_catalog)} with status: #{last_status}"
-    message = "Last run at #{Time.parse(last_catalog)} with status: #{last_status}\nPlease check https://#{@options[:puppetboard]}/node/#{@options[:node]}" if last_status == 'failed'
-    status  = [0, 'OK'] if last_status == 'unchanged'
-    status  = [0, 'OK'] if last_status == 'changed'
-    status  = [2, 'CRITICAL'] if last_status == 'failed'
-  end
+  message = "Last run at #{Time.parse(last_catalog)} with status: #{last_status}"
+  message = "Last run at #{Time.parse(last_catalog)} with status: #{last_status}\nPlease check https://#{@options[:puppetboard]}/node/#{@options[:node]}" if last_status == 'failed'
+  status  = [0, 'OK'] if last_status == 'unchanged'
+  status  = [0, 'OK'] if last_status == 'changed'
+  status  = [2, 'CRITICAL'] if last_status == 'failed'
 end
 
 puts "#{status[1]} - #{message}"
