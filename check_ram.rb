011f5efb85095b5c8e4cbebe0f875ef34bfddad2
cruelsmith <92088441+cruelsmith@users.noreply.github.com>
2024-01-28 17:16:47 +0100

    Speedup runtime of check_ram.rb and check_swap.rb

--- check_ram.rb
+++ check_ram.rb
@@ -1,37 +1,52 @@
 #! /opt/puppetlabs/puppet/bin/ruby
 
-# Author: Robert Waffen <robert.waffen@pixelpark.com>
-# Date:   2020-03-20
-
 require 'optparse'
+require 'json'
 
-available       = %x(/opt/puppetlabs/puppet/bin/facter memory.system.available).strip!
-available_bytes = %x(/opt/puppetlabs/puppet/bin/facter memory.system.available_bytes).strip!
-used            = %x(/opt/puppetlabs/puppet/bin/facter memory.system.used).strip!
-used_bytes      = %x(/opt/puppetlabs/puppet/bin/facter memory.system.used_bytes).strip!
-total           = %x(/opt/puppetlabs/puppet/bin/facter memory.system.total).strip!
-total_bytes     = %x(/opt/puppetlabs/puppet/bin/facter memory.system.total_bytes).strip!
+STATES = {
+  0 => 'OK',
+  1 => 'WARNING',
+  2 => 'CRITICAL',
+  3 => 'UNKOWN'
+}.freeze
 
 @options = {}
-
 OptionParser.new do |opts|
-  opts.on("-w WARNING")  { |warning| @options[:warning] = warning }
-  opts.on("-c CRITICAL") { |critical| @options[:critical] = critical }
+  opts.on('-w WARNING', OptionParser::DecimalNumeric)  { |warning| @options[:warning] = warning.to_f }
+  opts.on('-c CRITICAL', OptionParser::DecimalNumeric) { |critical| @options[:critical] = critical.to_f }
 end.parse!
+if @options[:warning].nil? || @options[:critical].nil?
+  puts "#{STATES[3]} - warning and critical thresholds not defined"
+  exit 3
+end
 
-warn = @options[:warning].to_f
-crit = @options[:critical].to_f
+begin
+  facter = JSON.parse(%x(/opt/puppetlabs/puppet/bin/facter --json memory.system))
+rescue StandardError => e
+  puts "#{STATES[3]} - errors on collecting memory stats\n#{e.message}"
+  exit 3
+end
 
-used_perc      = ((used_bytes.to_f / total_bytes.to_f) * 100).round(2)
-available_perc = ((available_bytes.to_f / total_bytes.to_f) * 100).round(2)
+used_perc      = ((facter['memory.system']['used_bytes'].to_f / facter['memory.system']['total_bytes'].to_f) * 100).round(2)
+available_perc = ((facter['memory.system']['available_bytes'].to_f / facter['memory.system']['total_bytes'].to_f) * 100).round(2)
 
-status = [0, 'OK']       if used_perc < warn
-status = [1, 'WARNING']  if used_perc >= warn
-status = [2, 'CRITICAL'] if used_perc >= crit
-
-output   = "#{status[1]} - RAM: Total: #{total} - Used: #{used} / #{used_perc}% - Available: #{available} / #{available_perc}%"
-perfdata = "Used=#{used_bytes};;; Available=#{available_bytes};;;"
+status = if used_perc >= @options[:critical]
+           2
+         elsif used_perc >= @options[:warning]
+           1
+         elsif used_perc < @options[:warning]
+           0
+         end
+output = [
+  STATES[status],
+  "RAM: Total: #{facter['memory.system']['total']}",
+  "Used: #{facter['memory.system']['used']} / #{used_perc}%",
+  "Available: #{facter['memory.system']['available']} / #{available_perc}%"
+].join(' - ')
+perfdata = [
+  "Used=#{facter['memory.system']['used_bytes']};;;",
+  "Available=#{facter['memory.system']['available_bytes']};;;"
+].join(' ')
 
 puts "#{output} | #{perfdata}"
-
-exit status[0]
+exit status
--- check_swap.rb
+++ check_swap.rb
@@ -1,41 +1,52 @@
 #! /opt/puppetlabs/puppet/bin/ruby
 
-# Author: Robert Waffen <robert.waffen@pixelpark.com>
-# Date:   2020-11-10
-
-# ./check_swap.rb -w 80 -c 90
-
-# OK - SWAP: Total: 8.00 GiB - Used: 627.72 MiB / 7.66% - Available: 7.39 GiB / 92.34% | Used=658210816;;; Available=7931719680;;;
-
 require 'optparse'
+require 'json'
 
-available       = %x(/opt/puppetlabs/puppet/bin/facter memory.swap.available).strip!
-available_bytes = %x(/opt/puppetlabs/puppet/bin/facter memory.swap.available_bytes).strip!
-used            = %x(/opt/puppetlabs/puppet/bin/facter memory.swap.used).strip!
-used_bytes      = %x(/opt/puppetlabs/puppet/bin/facter memory.swap.used_bytes).strip!
-total           = %x(/opt/puppetlabs/puppet/bin/facter memory.swap.total).strip!
-total_bytes     = %x(/opt/puppetlabs/puppet/bin/facter memory.swap.total_bytes).strip!
+STATES = {
+  0 => 'OK',
+  1 => 'WARNING',
+  2 => 'CRITICAL',
+  3 => 'UNKOWN'
+}.freeze
 
 @options = {}
-
 OptionParser.new do |opts|
-  opts.on("-w WARNING")  { |warning| @options[:warning] = warning }
-  opts.on("-c CRITICAL") { |critical| @options[:critical] = critical }
+  opts.on('-w WARNING', OptionParser::DecimalNumeric)  { |warning| @options[:warning] = warning.to_f }
+  opts.on('-c CRITICAL', OptionParser::DecimalNumeric) { |critical| @options[:critical] = critical.to_f }
 end.parse!
+if @options[:warning].nil? || @options[:critical].nil?
+  puts "#{STATES[3]} - warning and critical thresholds not defined"
+  exit 3
+end
 
-warn = @options[:warning].to_f
-crit = @options[:critical].to_f
+begin
+  facter = JSON.parse(%x(/opt/puppetlabs/puppet/bin/facter --json memory.swap))
+rescue StandardError => e
+  puts "#{STATES[3]} - errors on collecting memory stats\n#{e.message}"
+  exit 3
+end
 
-used_perc      = ((used_bytes.to_f / total_bytes.to_f) * 100).round(2)
-available_perc = ((available_bytes.to_f / total_bytes.to_f) * 100).round(2)
+used_perc      = ((facter['memory.swap']['used_bytes'].to_f / facter['memory.swap']['total_bytes'].to_f) * 100).round(2)
+available_perc = ((facter['memory.swap']['available_bytes'].to_f / facter['memory.swap']['total_bytes'].to_f) * 100).round(2)
 
-status = [0, 'OK']       if used_perc < warn
-status = [1, 'WARNING']  if used_perc >= warn
-status = [2, 'CRITICAL'] if used_perc >= crit
-
-output   = "#{status[1]} - SWAP: Total: #{total} - Used: #{used} / #{used_perc}% - Available: #{available} / #{available_perc}%"
-perfdata = "Used=#{used_bytes};;; Available=#{available_bytes};;;"
+status = if used_perc >= @options[:critical]
+           2
+         elsif used_perc >= @options[:warning]
+           1
+         elsif used_perc < @options[:warning]
+           0
+         end
+output = [
+  STATES[status],
+  "SWAP: Total: #{facter['memory.swap']['total']}",
+  "Used: #{facter['memory.swap']['used']} / #{used_perc}%",
+  "Available: #{facter['memory.swap']['available']} / #{available_perc}%"
+].join(' - ')
+perfdata = [
+  "Used=#{facter['memory.swap']['used_bytes']};;;",
+  "Available=#{facter['memory.swap']['available_bytes']};;;"
+].join(' ')
 
 puts "#{output} | #{perfdata}"
-
-exit status[0]
+exit status
