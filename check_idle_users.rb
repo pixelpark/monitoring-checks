3f66cc45e1dbaec1e27592d1c98b03a4e2cad9f6
cruelsmith <92088441+cruelsmith@users.noreply.github.com>
2024-05-30 20:03:21 +0200

    check_idle_users.rb: replace awk and facter dependency

--- check_idle_users.rb
+++ check_idle_users.rb
@@ -4,50 +4,49 @@
 # Date:   2020-05-15
 
 require 'optparse'
+require 'rbconfig'
 
-operatingsystem = %x( /opt/puppetlabs/bin/facter operatingsystem ).strip!
+who_cmd = case RbConfig::CONFIG['host_os']
+          when /solaris|sunos/i
+            'gwho'
+          else
+            'who'
+          end
 
-if operatingsystem == 'Solaris'
-  who_cmd = "gwho"
-else
-  who_cmd = "who"
+loggedin = %x(#{who_cmd}).lines(chomp: true).map do |line|
+  line.split.select.with_index do |_, index|
+    [0, 2].include?(index)
+  end
 end
 
-loggedin = %x( #{who_cmd} | awk '{print $1,$3}' ).strip!
-
-if loggedin.nil?
-  loggedin = Array.new
-else
-  loggedin = loggedin.split(/\n/)
-end
-
-date = Time.now.strftime("%Y-%m-%d")
+date = Time.now.strftime('%Y-%m-%d')
 idle_user = []
 
-@options = {}
+@options = {
+  warning: 1,
+  critical: 1
+}
 
 OptionParser.new do |opts|
-  opts.on("-w WARNING")  { |warning|  @options[:warning]  = warning  }
-  opts.on("-c CRITICAL") { |critical| @options[:critical] = critical }
-  opts.on("-e", TrueClass) { |extended| @options[:extended] = extended.nil? ? true : extended }
+  opts.on('-w WARNING')    { |warning|  @options[:warning]  = warning  }
+  opts.on('-c CRITICAL')   { |critical| @options[:critical] = critical }
+  opts.on('-e', TrueClass) { |extended| @options[:extended] = extended.nil? ? true : extended }
 end.parse!
 
 warn = @options[:warning].to_i
 crit = @options[:critical].to_i
 ext  = @options[:extended]
 
-loggedin.each do |user|
-  if user.split(' ')[1] != date
-    idle_user << user
-  end
-end unless loggedin.empty?
+loggedin.each do |entry|
+  idle_user << entry.join(' ') unless entry[1] == date
+end
 
 status = [0, 'OK']       if idle_user.count < warn
 status = [1, 'WARNING']  if idle_user.count >= warn
 status = [2, 'CRITICAL'] if idle_user.count >= crit
 
 output = "#{status[1]} - Idle Users: #{idle_user.count}" unless ext
-output = "#{status[1]} - Idle Users: #{idle_user.count} - Extended: #{idle_user.join(",")}" if ext
+output = "#{status[1]} - Idle Users: #{idle_user.count} - Extended: #{idle_user.join(',')}" if ext
 perfdata = "loggedin=#{idle_user.count};#{warn};#{crit};"
 
 puts "#{output} | #{perfdata}"
