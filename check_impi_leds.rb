0b0c3478f12ba205d14c811e0b9fd3e49056cd6f
cruelsmith <92088441+cruelsmith@users.noreply.github.com>
2024-05-30 20:45:16 +0200

    check_impi_leds.rb: replace awk and grep dependency

--- check_impi_leds.rb
+++ check_impi_leds.rb
@@ -3,38 +3,49 @@
 # Author: Robert Waffen <robert.waffen@pixelpark.com>
 # Date:   2019-08-09
 
-system_name  = %x( /opt/puppetlabs/bin/facter dmi.product.name | awk '{print $2,$3}' ).strip!
+system_name = %x(/opt/puppetlabs/bin/facter dmi.product.name).chomp.split.select.with_index do |_, index|
+  [1, 2].include?(index)
+end.join(' ')
 
 impi_tool = '/usr/bin/ipmitool'     if File.exist?('/usr/bin/ipmitool')
 impi_tool = '/usr/sbin/ipmitool'    if File.exist?('/usr/sbin/ipmitool')
 impi_tool = '/usr/sfw/bin/ipmitool' if File.exist?('/usr/sfw/bin/ipmitool')
+if impi_tool.nil?
+  puts 'UNKOWN - Could not found ipmitool binary'
+  exit 3
+end
 
 sudo = '/usr/bin/sudo' if File.exist?('/usr/bin/sudo')
 sudo = '/opt/csw/bin/sudo' if File.exist?('/opt/csw/bin/sudo')
+if sudo.nil?
+  puts 'UNKOWN - Could not found sudo binary'
+  exit 3
+end
 
-impi_version = %x( #{impi_tool} -V ).strip!
+impi_version = %x(#{impi_tool} -V).strip
 
 case system_name.downcase
-when "blade x6250", "blade x6270", "fire x4150", "fire x4270", "server x6-2", "server x7-2"
+when 'blade x6250', 'blade x6270', 'fire x4150', 'fire x4270', 'server x6-2', 'server x7-2'
   led_key        = 'sbled'
   impi_interface = 'bmc'
-when "fire x4100", "fire x4200", "fire x4600"
-  led_key        = "led"
+when 'fire x4100', 'fire x4200', 'fire x4600'
+  led_key        = 'led'
   impi_interface = 'bmc'
 else
   led_key        = 'sbled'
   impi_interface = 'open'
 end
 
-leds_on    = %x( #{sudo} #{impi_tool} -I #{impi_interface} -U root sunoem #{led_key} get 2>/dev/null | grep -i SERVICE | grep -cv OFF ).strip!
+leds = %x(#{sudo} #{impi_tool} -I #{impi_interface} -U root sunoem #{led_key} get 2>/dev/null).lines(chomp: true).grep(/SERVICE/i)
 
-if leds_on == "0"
-  puts "OK - All Service LEDs are Off"
+if leds.count { |line| line !~ /OFF/i } == '0'
+  puts 'OK - All Service LEDs are Off'
+  puts leds
   puts impi_version
   exit 0
 else
-  puts "CRITICAL - A Service LED is On"
-  puts %x( #{sudo} #{impi_tool} -I #{impi_interface} -U root sunoem #{led_key} get 2>/dev/null | grep -i SERVICE | grep -v OFF ).strip!
+  puts 'CRITICAL - A Service LED is On'
+  puts leds
   puts impi_version
   exit 2
 end
