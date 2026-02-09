85f22944d96513f0102d4be43fa581c6fb111016
cruelsmith <92088441+cruelsmith@users.noreply.github.com>
2026-02-09 16:18:00 +0100

    Add `check_file_exists.rb`

---
+++ check_file_exists.rb
@@ -0,0 +1,80 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+
+require 'optparse'
+require 'shellwords'
+
+STATES = {
+  0 => 'OK',
+  1 => 'WARNING',
+  2 => 'CRITICAL',
+  3 => 'UNKOWN'
+}.freeze
+
+options = {
+  file_path: [],
+  crit: 1,
+  warn: 1,
+  showdata: false,
+  perfdata: true
+}
+OptionParser.new do |opts|
+  opts.on('-f PATH', '--file PATH', String,
+          'File path to check against.',
+          'Supports glob syntax and repeated usage.') do |value|
+    options[:file_path] << value
+  end
+  opts.on('-c INTEGER', '--crit INTEGER', Integer,
+          'More that this count of files will return critical state.',
+          'If 0 or less than warn, state logic is reversed.',
+          "Default: #{options[:crit]}") do |value|
+    options[:crit] = value
+  end
+  opts.on('-w INTEGER', '--warn INTEGER', Integer,
+          'More than this count of files will return warning state.',
+          'If more than crit, state logic is reversed.',
+          "Default: #{options[:warn]}") do |value|
+    options[:warn] = value
+  end
+  opts.on('--[no-]showdata', TrueClass,
+          "Default: #{options[:showdata]}") do |value|
+    options[:showdata] = value
+  end
+  opts.on('-p', '--[no-]perfdata', TrueClass,
+          "Default: #{options[:perfdata]}") do |value|
+    options[:perfdata] = value
+  end
+  opts.on('-h', '--help', 'Prints this help') do
+    puts opts
+    exit
+  end
+end.parse!
+
+files = Dir.glob(options[:file_path])
+
+exitstate = if options[:crit] < options[:warn] || options[:crit] == 0
+              if files.count <= options[:crit]
+                2
+              elsif files.count <= options[:warn]
+                1
+              else
+                0
+              end
+            else
+              if files.count >= options[:crit]
+                2
+              elsif files.count >= options[:warn]
+                1
+              else
+                0
+              end
+            end
+result = if files.empty?
+           'no matching files found'
+         else
+           "#{files.count} files found"
+         end
+result += "|file_count=#{files.count};#{options[:warn]};#{options[:crit]};0" if options[:perfdata]
+
+puts "#{STATES[exitstate]} - #{result}"
+puts files.map(&:shellescape).join("\n") if options[:showdata]
+exit exitstate
