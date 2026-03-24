196b31697429a0d3d53284b017b418cf39063672
cruelsmith <92088441+cruelsmith@users.noreply.github.com>
2026-03-24 09:58:09 +0100

    `verify_update_state.rb` - added
    Replace heuristic attemt with a post-transaction-actions hook to when all updates has been installed and use this date as last upgrade date.

--- check_dnf_update.rb
+++ check_dnf_update.rb
@@ -13,25 +13,36 @@ STATES = {
 
 options = {
   warning: 90,
-  critical: 91
+  critical: 91,
+  state_file: '/var/lib/dnf/verify_update_state.log'
 }
 
 OptionParser.new do |opts|
   opts.on('-w WARNING', Integer) { |warning| options[:warning] = warning }
   opts.on('-c CRITICAL', Integer) { |critical| options[:critical] = critical }
+  opts.on('--state-file PATH', String) { |path| options[:state_file] = path }
 end.parse!
+
+last_upgrade = Time.parse('1970-01-01T00:00:00Z')
+unless File.readable?(options[:state_file])
+  puts "#{status[3]} - state file not existing or readable: #{options[:state_file]}"
+  exit 3
+end
+File.open(options[:state_file], 'r') do |file|
+  File.foreach(file) do |line|
+    line = line.split
+    next unless line[1] == '0'
+
+    break if (last_upgrade = Time.parse(line[0]))
+  end
+end
+
+current = Time.now.utc
+threshold_crit = Time.utc(last_upgrade.year, last_upgrade.month, last_upgrade.day, 0, 0, 0) + (options[:critical] * 86_400)
+threshold_warn = Time.utc(last_upgrade.year, last_upgrade.month, last_upgrade.day, 0, 0, 0) + (options[:warning] * 86_400)
 
 # `/bin/dnf list updates` would also show versionlocked packages
 check_update, check_update_status = Open3.capture2e('/bin/dnf --quiet --cacheonly check-update')
-
-kernel, = Open3.capture2e('/bin/dnf repoquery --quiet --cacheonly --qf=\'%{name}-%{version}-%{release}.%{arch} %{installtime}\' --installed kernel') # rubocop:disable Style/FormatStringToken
-kernel = kernel.lines(chomp: true).to_h { |x| x.split(nil, 2) }.max { |a, b| a[1] <=> b[1] }.last
-kernel = Time.parse("#{kernel} UTC")
-
-current = Time.now.utc
-threshold_crit = Time.utc(kernel.year, kernel.month, kernel.day, 10, 0, 0) + (options[:critical] * 86_400)
-threshold_warn = Time.utc(kernel.year, kernel.month, kernel.day, 10, 0, 0) + (options[:warning] * 86_400)
-
 _, needs_restarting = Open3.capture2e('/usr/bin/needs-restarting --reboothint')
 
 status = if (current >= threshold_crit && check_update_status.exitstatus == 100) || !needs_restarting.success?
@@ -43,10 +54,10 @@ status = if (current >= threshold_crit && check_update_status.exitstatus == 100)
          else
            [3, 'UNKOWN']
          end
-output = "#{status[1]} -"
-output += ' System needs restarting!' unless needs_restarting.success?
-output += " Last kernel install: #{kernel}"
-output += ", Updates available:\n#{check_update}" unless check_update_status.success?
+output = status[1]
+output += ' - System needs restarting!' unless needs_restarting.success?
+output += " - Last upgrade: #{last_upgrade}"
+output += "\nUpdates available:\n#{check_update}" unless check_update_status.success?
 
 puts output
 
